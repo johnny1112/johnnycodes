d05d9a8f4352e00566857fb48cbace5cfb46c252
johnny1112 <43337475+johnny1112@users.noreply.github.com>
2019-03-05 12:04:31 +0530

    Create lcm.py

---
+++ lcm.py
@@ -0,0 +1,11 @@
+x=int(input("num1:"))
+y=int(input("num2"))
+if (x>y)
+minimum1=x
+else
+minimum1=y
+while(1)
+if(minimum1%x==0 and minimum1%y==0)
+print ("output:",minimum1)
+break
+minimum1=minimum1+1
